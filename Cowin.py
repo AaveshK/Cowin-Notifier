8821ac307fc167db6736b2bb986596482f95aa50
Aavesh Kumar Sinha <aks20000@hotmail.com>
2021-05-14 23:15:04 +0530

    Added comments and minor changes

--- Cowin.py
+++ Cowin.py
@@ -12,14 +12,14 @@ url= "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin
 pin=input("Enter your pin ") 
 url=url+pin+"&date="
 url=url+d1
-#url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=834001&date=14-05-2021"
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 tl = Timeloop()
 hr=ToastNotifier()
 age = int(input("What's your age? "))
 
-@tl.job(interval=timedelta(seconds=60))
+@tl.job(interval=timedelta(seconds=60)) #Change the value to play with the frequency of updation
 def sample_job_every_60s():
      print ("60s job current time : {}".format(time.ctime()))
 
@@ -42,14 +42,10 @@ def sample_job_every_60s():
                     print("\n")
                     count += 1
                     if count ==1: hr.show_toast("Vaccine slot alert", "Earliest slot available at "+i["name"]+" on "+str(j["date"]),duration=40)
+                         #keep the above line outside (if) to receive all the available slots via notification
                else:
                     continue
-     #if count == 0:
-          #hr.show_toast("Vaccine slot alert","No vacant slot",duration=10)
-          #print("No vacant slot")
-     #else:
-          #print("Total options available: "+str(count))
-          #hr.show_toast("Vaccine slot alert", "Slot available at "+i["name"]+" on "+str(j["date"]),duration=40)
-
+     if count>0:
+          print("Total options available: "+str(count))
 if __name__ == "__main__":
      tl.start(block=True)
